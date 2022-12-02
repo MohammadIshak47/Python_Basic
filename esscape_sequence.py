e86820bcce78df0e977c15b08df67a4c0b230228
MohammadIshak <ishak1998bd@gmail.com>
2022-12-02 08:48:28 +0530

    updated now

---
+++ esscape_sequence.py
@@ -0,0 +1,12 @@
+# print("Bangladesh is our motherland. I \"Mohammad Ishak\" live in this country")
+# print('Focus on your Goal . Think how can you complete your final goal \'Actual Goal\'')
+# print('I\'m Mohammad Ishak. You know about me How you work in different places.')
+
+# print("Helllo Ishak, How are You? \nAre coming from Bangladesh")
+
+print("Your name is \t Mohammad Ishak") # \t create space create between two words
+print("My thinking capability is not perfect\\") # \\ create single backlash(\)
+
+print("Who are you taking risk in a crictical situation\\\\") #\\\\ create double backlash \\
+    
+    
