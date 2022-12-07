d3f03c1dc202b63c459008f8b1b8652b5f0efa18
MohammadIshak47 <ishak1998bd@gmail.com>
2022-12-07 22:24:13 +0530

    updated function code

--- part_03/functions.py
+++ part_03/functions.py
@@ -1,4 +1,12 @@
 
+'''
+
+###
+def add(a,b):
+    return a+b
+
+print(add(33,44)) 
+
 def sum_num(a,b,c):
     return a+b+c
 
@@ -8,3 +16,6 @@ b = int(input("Enter second number : "))
 c = int(input("Enter third number : "))
 
 print("The result of sum is : ",sum_num(a, b, c))
+
+'''
+   
