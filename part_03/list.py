a6426a0b1648c5823b7c869bc87b5cb11ebfa989
MohammadIshak47 <ishak1998bd@gmail.com>
2022-12-08 10:56:59 +0530

    added list

---
+++ part_03/list.py
@@ -0,0 +1,16 @@
+# a = ['Dhaka','Barisal','Rajshahi','Sylhet','Chittagong']
+a = [1,2,3,"ISHAK",'Dhaka',3.44,'num1']
+# print(a)
+# print(a[3])
+# print(a[:3])
+# print(a[3:])
+# print(a[2:4])
+# print(a[:-1])
+# print(a[-1:0])
+
+# a[2] = "red"
+a[2:]  = ['yellow','black','white','green'] # to change value
+print(a)
+
+## Add Data into the list
+
