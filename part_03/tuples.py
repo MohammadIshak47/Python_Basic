0ba7e7420bfa2f765b91141eaab8d822494598d7
MohammadIshak47 <ishak1998bd@gmail.com>
2022-12-13 15:58:35 +0530

    updated tuples file

---
+++ part_03/tuples.py
@@ -0,0 +1,36 @@
+## Tuples are immutable that meanse it not changeable or updateable
+## after element without comma it not would be tuple
+'''
+# tuples = (2)
+tuples = (2,) # it would be tuples
+print(type(tuples))
+a = ("bangla",)  # it would be tuples
+print(type(a))
+
+## tuple without parenthesis
+tuple1 = 'bangla','english','science','religious study'
+print(type(tuple1))
+
+## tuple unpacking
+
+name = ('rasel','faisal','mursalin')
+name1,name2,name3 = name
+print(name1)
+print(name2)
+print(name3)
+
+### List inside tuples
+
+color = ('red',['green','purple','orange'])
+color[1].pop()
+color[1].append('color choice')
+print(color)
+
+'''
+
+
+
+
+
+
+
