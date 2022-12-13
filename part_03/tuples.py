74624b328254bf01375b61e67e317dc8ec392412
MohammadIshak47 <ishak1998bd@gmail.com>
2022-12-13 21:45:11 +0530

    updated tuples file & info

--- part_03/tuples.py
+++ part_03/tuples.py
@@ -1,5 +1,11 @@
 ## Tuples are immutable that meanse it not changeable or updateable
+## we cannot change it values once it created
+## but can add ,delete data from tuple list which is present inside tuple
+## tuple is faster than list
 ## after element without comma it not would be tuple
+## tuples are ordered collection of items
+## No append ,no pop , no insert, no remove
+## only count and index
 '''
 # tuples = (2)
 tuples = (2,) # it would be tuples
@@ -26,7 +32,6 @@ color[1].pop()
 color[1].append('color choice')
 print(color)
 
-'''
 
 ## Function return two values 
 
@@ -39,6 +44,13 @@ def func(num1,num2):
 addition,multiplication = (30,40)
 print(addition)
 print(multiplication)
+
+'''
+
+# nums = tuple(range(1,10))
+# print(nums)
+
+
 
 
 
