8b571e0205f854d691b8bd3be55baaed006fe319
MohammadIshak47 <ishak1998bd@gmail.com>
2022-12-14 13:26:51 +0530

    updated problems with solution

--- part_03/dictionaries.py
+++ part_03/dictionaries.py
@@ -28,7 +28,6 @@ info_of_person = {
 }
 
 print(info_of_person['favorite_movies'])
-'''
 
 ## How to add data into empty dictionaries
 
@@ -67,5 +66,76 @@ user_info ={
 # print(user_items)
 # print(type(user_items))
 
-for key,value in user_info.items():
-    print(f"the key is {key} and value is {value}")
+# for key,value in user_info.items():
+#     print(f"the key is {key} and value is {value}")
+
+## pop method
+
+# popped_items = user_info.pop('gender')
+# print(f"pop item is {popped_items}")
+# print(popped_items)
+
+
+## popitem
+
+pop_item = user_info.popitem()
+print(user_info)
+print(pop_item)
+
+user_info = {
+    'Name':'Mohammad Ishak',
+    'Gender':'Male',
+    'favs_time':'winter',
+    'favs_books': ('Bela furabar age ','by arif azad'),
+    'favs_tunes': ('isq','pyaar thora')
+}
+
+more_info = {'Name':'Foisal Imtiaz','city':'Dhaka','Hobbies':['reading books','videos']}
+
+user_info.update(more_info)
+print(user_info)
+
+## fromkeys
+
+user_info = {'name':'Ishak','age':'26','gender':'male','gender':'female'} ## in 
+#dictionaries there not will be same key .it will be print only one key and overrite with end key
+# user_info = dict.fromkeys(('name','age','gender'),'unknown') # unknown value will enter the name ,age and gender keys
+# user_info = dict.fromkeys(range(1,10),'unknown')
+## Get Method
+# print(user_info.get('name'))# we can access keys &  get keys value using get method
+# print(user_info)
+
+print(user_info.get('names')) ## if not key found then it return None
+print(user_info.get('names','not found')) # Now it will return not found
+
+print(user_info)
+
+# if user_info.get('name'):
+#     print("This method is present here")
+# else:
+#     print("This method is not present here")
+## None means no value is exist here
+# # if None ...False else True
+
+# # user_info.clear()
+# user_info1 = user_info.copy()
+# user_info1.popitem()
+# print(user_info1)        
+
+
+##problem_02: Define a function that takes a number(n).return a dictionary containing
+# cube of numebers from 1 to n. (cube menas 1**3)
+
+## Solution:
+##cube finder
+
+def cube_finder(n):
+    cubes = {}
+    for i in range(1,n+1):
+        cubes[i] = i**3
+    return cubes
+print(cube_finder(5)) 
+
+'''
+       
+
