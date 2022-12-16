1c6e71f1f152a5149049fe3a334f0eecb6c59fe8
MohammadIshak47 <ishak1998bd@gmail.com>
2022-12-16 10:27:34 +0530

    updated args files

--- part_04_pro_level/args.py
+++ part_04_pro_level/args.py
@@ -12,8 +12,6 @@ def sum_all(*args):
 
 print(sum_all(20,12,14))
 
-'''
-
 ## *args with normal parameter
 
 # def multiply_all(*args):
@@ -24,3 +22,37 @@ def multiply_all(num,*args): ## here if we enter normal parameter (num),then *ar
     return total
 
 print(multiply_all(2,3,4,3))
+
+
+#### args as argument
+
+def multiply_all(*args):
+    total = 1
+    for i in args:
+        total*=i
+    return total
+
+list1 = [2,3,4] 
+# print(multiply_all(list1)) ## it will multiply the list but not with the elements of list
+print(multiply_all(*list1)) # now it will unpack & multiply the elements of list
+
+'''
+
+## problem : define a function . Input num,iterable(tuple,list) containing numbers as args
+## Example :  nums = [1,2,3]
+## to_power(3,*nums)
+## output : list --[1**3,8,27]
+## if user didn't pass any args then give a user message 'hey you didn't pass args
+### else return list
+
+### Solution
+
+def to_power(num,*args):
+    if args:
+        return [i**num for i in args]
+    else:
+        return 'You didn\'t pass any args'    
+        
+nums = [1,2,3]
+# print(to_power(4,*nums))
+print(to_power(3,*[1,2,4]))
