125b499c8e1003ad26b4676f10213c4e72498b3f
MohammadIshak47 <ishak1998bd@gmail.com>
2022-12-20 22:41:51 +0530

    updated enumerate files

---
+++ part_04_pro_level/enumerate.py
@@ -0,0 +1,17 @@
+
+'''
+### We use enumerate function with for loop to track position of our item  in iterable
+
+
+'''
+## How can we do this without enumerate function
+
+names = ['bangla','english','mathematics','science']
+
+position = 0
+
+for name in names:
+    print(name)
+    position+=1
+    
+
