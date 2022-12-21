35ce87c546c1061705111ee2d039e8e13aa2b68d
MohammadIshak47 <ishak1998bd@gmail.com>
2022-12-21 13:23:39 +0530

    updated more info

--- part_04_pro_level/zip_function.py
+++ part_04_pro_level/zip_function.py
@@ -29,8 +29,6 @@ l1,l2 = (list(zip(*l))) ## here l list value enter in l1,l2 and convert only two
 print(l1)
 print(l2)
 
-'''
-
 l1 = [1,2,3,4,5]
 l2 = [6,7,8,9,10]
 
@@ -39,5 +37,29 @@ new_list = []
 for pair in zip(l1,l2):
     new_list.append(max(pair))
 print(new_list) ## here will print maixmum value list like [6,7,8,9,10]
+
+'''
+
+## problem_01 : Define a function take any no of lists containing number 
+##  [1,2,3],[4,5,6],[7,8,9]
+## return average (1+4+7)/3 ,(2+5+8)/3,(3+6+9)/3
+# Try to make this anonymous function in one line using lambda
+
+## Solution without using lambda
+
+# def average_finder(*args):
+#     average = []
+#     for pair in zip(*args):
+#         average.append(sum(pair)/len(pair))
+#     return average
+
+# print(average_finder([1,2,3],[4,5,6],[7,8,9]))
+
+### using lambda
+# average_finder = lambda *args : [sum(pair)/len(pair) for pair in zip(*args)]
+# print(average_finder([1,2,3],[4,5,6],[7,8,9]))
+#or
+print((lambda *args:[sum(pair)/len(pair) for pair in zip(*args)]) ([1,2,3],[4,5,6],[7,8,9]))
+
 
 
