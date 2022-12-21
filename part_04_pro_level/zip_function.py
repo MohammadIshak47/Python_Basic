b48a730a546482cc0c407b3045acf801393ac0fc
MohammadIshak47 <ishak1998bd@gmail.com>
2022-12-21 10:30:19 +0530

    updated zip files

---
+++ part_04_pro_level/zip_function.py
@@ -0,0 +1,16 @@
+'''
+### Zip function( How to access in user id and suppose it combind user to names)
+
+# user_id  = ('user1','user2','user3','user4')
+# user_id  = ('user1','user2','user3') ## if user less than names than it read 
+# #and print those user what is existed in user_id
+# names = ('Ishak','Irfan','Anas','Ayisha')
+# print(list(zip(user_id,names)))'
+
+'''
+
+user_info = [('a',1),('b',2)]
+print(dict(user_info))
+
+
+
