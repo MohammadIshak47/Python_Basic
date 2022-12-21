bd1c06d4360339f1ba48cfa6c8bc7ad2cabb8bad
MohammadIshak47 <ishak1998bd@gmail.com>
2022-12-21 20:40:26 +0530

    updated sorted files

--- part_04_pro_level/advance_min_max_func.py
+++ part_04_pro_level/advance_min_max_func.py
@@ -27,4 +27,19 @@ student1= [
 ]
 
 # print(max(student1, key = lambda item: item.get('score')))
-print(max(student1, key=lambda item:item.get('age')))
+# print(max(student1, key=lambda item:item.get('score'))['name']) ## only name will be given
+# print(min(student1, key=lambda item:item.get('score')))
+#print(max(student1, key=lambda item:item.get('age'))['name'])
+
+## another ## Complex datatype
+
+student2 = {
+    'Mohammad':{'score':90,'age':23}, # key then into dictionary
+    'Faisal':{'score':88,'age':25},
+    'Ayisha':{'score':92,'age':10}
+}
+
+# print(max(student2, key=lambda item : student2[item]['score'])) ## Here first we entered
+## key 'Mohammad ' then enterd its item
+
+print(min(student2, key=lambda item:student2[item]['age']))
