4d0e2889436bf3a4ffed4a724131c0d49c2c9c4d
MohammadIshak47 <ishak1998bd@gmail.com>
2022-12-21 11:12:13 +0530

    updated zip files more info

--- part_04_pro_level/zip_function.py
+++ part_04_pro_level/zip_function.py
@@ -7,10 +7,37 @@
 # names = ('Ishak','Irfan','Anas','Ayisha')
 # print(list(zip(user_id,names)))'
 
-'''
-
 user_info = [('a',1),('b',2)]
 print(dict(user_info))
 
+## With two name list combined
+user_id  = ('user1','user2','user3','user4')
+name1 = ('Ishak','Irfan','Anas','Ayisha')
+name2 =('Ahmed','Ahmed','Ahmed','Akther')
+
+print(list(zip(user_id,name1,name2)))
+
+### zip with *
+
+l1 = [1,2,3,4,5]
+l2 = [6,7,8,9,10]
+
+l = [(1,2),(5,6),(9,10)] ## if two value in just one list like (1,2) it will convert smaller list
+print(list(zip(*l)))
+
+l1,l2 = (list(zip(*l))) ## here l list value enter in l1,l2 and convert only two list
+print(l1)
+print(l2)
+
+'''
+
+l1 = [1,2,3,4,5]
+l2 = [6,7,8,9,10]
+
+new_list = []
+
+for pair in zip(l1,l2):
+    new_list.append(max(pair))
+print(new_list) ## here will print maixmum value list like [6,7,8,9,10]
 
 
