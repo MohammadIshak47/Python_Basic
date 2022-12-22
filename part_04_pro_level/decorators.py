7b6cdf0012fe4171214facf553e391cfb10d9572
MohammadIshak47 <ishak1998bd@gmail.com>
2022-12-22 07:58:45 +0530

    updated more info

--- part_04_pro_level/decorators.py
+++ part_04_pro_level/decorators.py
@@ -8,4 +8,20 @@
 
 ####  pass function as a argument 
 
+# def square(a):
+#     return a**2
+
+
+# l = [1,2,3,4,5]
+# print(list(map(square, l)))
+
+## custom function
+l = [1,2,3,4]
+def square_define(func,l):
+    new_list = []
+    for item in l:
+        new_list.append(func(item))
+    return new_list
+
+
 
