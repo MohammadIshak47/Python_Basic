8365257857d62c0fa20e338399ae11854fa3af23
MohammadIshak47 <ishak1998bd@gmail.com>
2022-12-24 20:55:09 +0530

    updated more info

--- OOP_Part_05/property_and_setter_decorder.py
+++ OOP_Part_05/property_and_setter_decorder.py
@@ -1,3 +1,27 @@
 ### We will discuss three problem existing 
 ## then we will solve them using setter ,setter deocorder
 
+
+class Phone:
+
+    def __init__(self,brand,model_name,price):
+        self.brand = brand
+        self.model_name = model_name
+        self._price = price
+        self.complete_specification = f"{self.brand} {self.model_name} and {self._price} "
+
+    def make_a_phone(self,phone_number):
+        return f"calling {phone_number}..."  
+
+    def full_name(self):
+        return f"{self.brand} {self.model_name}"
+
+
+phone1 = Phone('Apple', 'iphone14 pro', 123000)
+phone2 = Phone('Oppo', 's21', 30000)
+
+print(phone1.brand)
+print(phone1.model_name)
+print(phone1._price)
+print(phone1.full_name())
+print(phone1.complete_specification)
